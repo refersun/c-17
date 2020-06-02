7e0a778d824277a3fd10e7a1850cdf29e191b933
xuchaogitstore <refersun@163.com>
2020-06-02 18:44:44 +0800

    今天练习了array<>模板

--- text.cpp
+++ text.cpp
@@ -1,7 +1,13 @@
-#includ<iostream>
-
+#include<iostream>
+#include<array>
+using namespace std;
 int main()
 {
+	int arry[]={1,2,3,4};
+	char strc[]={"hello"};
+	array<int,12> values;
+	cout<<sizeof(values)<<endl<<endl;
+	
 
 }
 
